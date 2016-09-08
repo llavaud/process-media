53440f13b52fda7f85d0987de33a54532dbf0d14
llavaud <laurent.lavaud@ladtech.fr>
2016-09-08 11:28:51 +0200

    update dev branch

--- lib/Video.pm
+++ lib/Video.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use File::Basename qw/fileparse/;
+use File::Copy qw/copy/;
 use File::Path qw/make_path/;
 use Image::ExifTool qw/ImageInfo/;
 use Image::Magick;
@@ -17,7 +18,6 @@ sub new {
 
     # with threads, need to share specifically every sub hash
     $obj{'original'} = &share({});
-    $obj{'options'} = &share({});
     $obj{'final'} = &share({});
 
     bless \%obj, $class;
@@ -38,13 +38,15 @@ sub init {
     $obj->{'original'}->{'dir'} = $dir;
     $obj->{'original'}->{'extension'} = $ext;
 
-    $obj->{'options'} = $opts;
-
     # set final dir
     foreach (keys %{ $main::options{'format'} }) {
         next if $main::options{'format'}{$_}{'type'} ne 'video';
         $obj->{'final'}->{$_} = &share({});
-        $obj->{'final'}->{$_}->{'dir'} = $obj->{'original'}->{'dir'}.$_.'/videos/';
+        if (defined $main::options{'format'}{$_}{'output_dir'}) {
+            $obj->{'final'}->{$_}->{'dir'} = $obj->{'original'}->{'dir'}.$main::options{'format'}{$_}{'output_dir'}.'/';
+        } else {
+            $obj->{'final'}->{$_}->{'dir'} = $obj->{'original'}->{'dir'}.$_.'/';
+        }
     }
 
     $obj->{'final'}->{'extension'} = '.mp4';
@@ -57,7 +59,7 @@ sub rename {
 
     lock $obj;
 
-    if (defined $obj->{'options'}->{'keep_name'}) {
+    if ($main::options{'keep_name'} eq 'true') {
         $obj->{'final'}->{'name'} = $obj->{'original'}->{'name'};
     }
     else {
@@ -73,11 +75,15 @@ sub rename {
         $obj->{'final'}->{'name'} = $t->ymd('').'-'.$t->hms('');
     }
 
-    print "[$obj->{'original'}->{'path'}] Rename to \'$obj->{'final'}->{'name'}\'\n" if defined $obj->{'options'}->{'verbose'};
+    print "[$obj->{'original'}->{'path'}] Rename to \'$obj->{'final'}->{'name'}\'\n"
+        if $main::options{'verbose'} eq 'true';
 
     # set final path
-    foreach (split(',',$obj->{'options'}->{'format'})) {
-        $obj->{'final'}->{$_}->{'path'} = $obj->{'final'}->{$_}->{'dir'}.$obj->{'final'}->{'name'}.$obj->{'final'}->{'extension'};
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
+        $obj->{'final'}->{$_}->{'path'} = $obj->{'final'}->{$_}->{'dir'}
+                                         .$obj->{'final'}->{'name'}
+                                         .$obj->{'final'}->{'extension'};
     }
 
     return 1;
@@ -88,74 +94,92 @@ sub exist {
 
     lock $obj;
 
-    print "$obj->{'original'}->{'path'} -> $obj->{'final'}->{'name'}\n" unless defined $obj->{'options'}->{'verbose'};
+    print "$obj->{'original'}->{'path'} -> $obj->{'final'}->{'name'}\n"
+        if $main::options{'verbose'} eq 'false';
 
-    foreach my $f (split(',',$obj->{'options'}->{'format'})) {
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
 
-        if (-f $obj->{'final'}->{$f}->{'path'}) {
-            print "[$obj->{'final'}->{$f}->{'path'}] Already exist...\n" unless defined $obj->{'options'}->{'overwrite'};
-            $obj->{'final'}->{$f}->{'exist'} = 1;
+        if (-f $obj->{'final'}->{$_}->{'path'}) {
+            print "[$obj->{'final'}->{$_}->{'path'}] Already exist...\n"
+                if $main::options{'overwrite'} eq 'false';
+
+            $obj->{'final'}->{$_}->{'exist'} = 1;
         }
     }
 
     return 1;
 }
 
-sub encode {
+sub create {
     my $obj = shift;
 
     lock $obj;
 
-    foreach my $f (split(',',$obj->{'options'}->{'format'})) {
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
+        next if defined $obj->{'final'}->{$_}->{'exist'} and $main::options{'overwrite'} eq 'false';
 
-        next if defined $obj->{'final'}->{$f}->{'exist'} and not defined $obj->{'options'}->{'overwrite'};
+        print "[$obj->{'final'}->{$_}->{'path'}] Creating...\n" if $main::options{'verbose'} eq 'true';
 
-        make_path $obj->{'final'}->{$f}->{'dir'} unless -d $obj->{'final'}->{$f}->{'dir'};
-
-        print "[$obj->{'final'}->{$f}->{'path'}] Encoding...\n" if defined $obj->{'options'}->{'verbose'};
-
-        if ($f eq 'archive') {
-            if ($obj->{'options'}->{'codec'} eq 'x264') {
-                &execute($obj->{'final'}->{$f}->{'path'}, 'Failed to encode', "ffmpeg -y -loglevel warning -i $obj->{'original'}->{'path'} -codec:a copy -flags +global_header $obj->{'final'}->{$f}->{'path'}");
-            } else {
-                &execute($obj->{'final'}->{$f}->{'path'}, 'Failed to encode', "ffmpeg -y -loglevel error -i $obj->{'original'}->{'path'} -codec:a copy -codec:v libx265 -x265-params crf=23:log-level=error -flags +global_header $obj->{'final'}->{$f}->{'path'}");
-            }
-        }
-        elsif ($f eq 'web') {
-            if ($obj->{'options'}->{'codec'} eq 'x264') {
-                &execute($obj->{'final'}->{$f}->{'path'}, 'Failed to encode', "ffmpeg -y -loglevel warning -i $obj->{'original'}->{'path'} -codec:a copy -vf \"scale='if(gt(iw,ih),1024,trunc(oh*a/2)*2)':'if(gt(iw,ih),trunc(ow/a/2)*2,1024)'\" -flags +global_header $obj->{'final'}->{$f}->{'path'}");
-            } else {
-                &execute($obj->{'final'}->{$f}->{'path'}, 'Failed to encode', "ffmpeg -y -loglevel error -i $obj->{'original'}->{'path'} -codec:a copy -codec:v libx265 -x265-params crf=23:log-level=error -vf \"scale='if(gt(iw,ih),1024,trunc(oh*a/2)*2)':'if(gt(iw,ih),trunc(ow/a/2)*2,1024)'\" -flags +global_header $obj->{'final'}->{$f}->{'path'}");
-            }
-        }
+        make_path $obj->{'final'}->{$_}->{'dir'} unless -d $obj->{'final'}->{$_}->{'dir'};
+        copy("$obj->{'original'}->{'path'}", "$obj->{'final'}->{$_}->{'path'}");
     }
 
     return 1;
 }
 
-sub delete_tags {
+sub process {
     my $obj = shift;
 
     lock $obj;
 
-    foreach my $f (split(',',$obj->{'options'}->{'format'})) {
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
+        next if defined $main::options{'format'}{$_}{'reencode'} and $main::options{'format'}{$_}{'reencode'} eq 'false';
+        next if defined $obj->{'final'}->{$_}->{'exist'} and $main::options{'overwrite'} eq 'false';
 
-        next if defined $obj->{'final'}->{$f}->{'exist'} and not defined $obj->{'options'}->{'overwrite'};
+        print "[$obj->{'final'}->{$_}->{'path'}] Processing...\n" if $main::options{'verbose'} eq 'true';
 
-        # dont delete tags for archive format
-        next if $f eq 'archive';
+		my $cmd = "ffmpeg -nostdin -hide_banner -y -loglevel warning -i $obj->{'original'}->{'path'} -codec:a copy -flags +global_header";
 
-        print "[$obj->{'final'}->{$f}->{'path'}] Deleting tags...\n" if defined $obj->{'options'}->{'verbose'};
+		if (defined $main::options{'format'}{$_}{'codec'} and $main::options{'format'}{$_}{'codec'} eq 'x265') {
+			$cmd .= " -codec:v libx265 -x265-params crf=23:log-level=error";
+		}
+
+		if (defined $main::options{'format'}{$_}{'resize'}) {
+            $cmd .= " -vf \"scale='if(gt(iw,ih),$main::options{'format'}{$_}{'resize'},trunc(oh*a/2)*2)':'if(gt(iw,ih),trunc(ow/a/2)*2,$main::options{'format'}{$_}{'resize'})'\"";
+        }
+
+        $cmd .= " $obj->{'final'}->{$_}->{'path'}";
+
+        &execute($obj->{'final'}->{$_}->{'path'}, 'Failed to encode', $cmd);
+    }
+
+    return 1;
+}
+
+sub strip {
+    my $obj = shift;
+
+    lock $obj;
+
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
+        next if not defined $main::options{'format'}{$_}{'strip'};
+        next if defined $obj->{'final'}->{$_}->{'exist'} and $main::options{'overwrite'} eq 'false';
+
+        print "[$obj->{'final'}->{$_}->{'path'}] Stripping...\n" if $main::options{'verbose'} eq 'true';
 
         # remove all tags
         my $exif = new Image::ExifTool;
         my ($ret, $err) = $exif->SetNewValue('*');
         if (defined $err) {
-            warn "[$obj->{'final'}->{$f}->{'path'}] Failed to remove tags, $err";
+            warn "[$obj->{'final'}->{$_}->{'path'}] Failed to remove tags, $err";
             return 0;
         }
-        unless ($exif->WriteInfo($obj->{'final'}->{$f}->{'path'})) {
-            warn "[$obj->{'final'}->{$f}->{'path'}] Failed to write, ".$exif->GetValue('Error');
+        unless ($exif->WriteInfo($obj->{'final'}->{$_}->{'path'})) {
+            warn "[$obj->{'final'}->{$_}->{'path'}] Failed to write, ".$exif->GetValue('Error');
             return 0;
         }
     }
@@ -168,28 +192,26 @@ sub thumbnail {
 
     lock $obj;
 
-    foreach my $f (split(',',$obj->{'options'}->{'format'})) {
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
+        next if not defined $main::options{'format'}{$_}{'thumbnail'} or $main::options{'format'}{$_}{'thumbnail'} eq 'false';
+        next if defined $obj->{'final'}->{$_}->{'exist'} and $main::options{'overwrite'} eq 'false';
 
-        next if defined $obj->{'final'}->{$f}->{'exist'} and not defined $obj->{'options'}->{'overwrite'};
+        print "[$obj->{'final'}->{$_}->{'path'}] Generating thumbnail...\n" if $main::options{'verbose'} eq 'true';
 
-        # dont create thumbnail for archive format
-        next if $f eq 'archive';
-
-        print "[$obj->{'final'}->{$f}->{'path'}] Generating thumbnail...\n" if defined $obj->{'options'}->{'verbose'};
-
-        &execute($obj->{'final'}->{$f}->{'path'}, 'Failed to create thumbnail', "ffmpeg -y -loglevel error -i $obj->{'final'}->{$f}->{'path'} -vframes 1 $obj->{'final'}->{$f}->{'dir'}$obj->{'final'}->{'name'}.jpg");
+        &execute($obj->{'final'}->{$_}->{'path'}, 'Failed to create thumbnail', "ffmpeg -y -loglevel error -i $obj->{'final'}->{$_}->{'path'} -vframes 1 $obj->{'final'}->{$_}->{'dir'}$obj->{'final'}->{'name'}.jpg");
 
         my $image = new Image::Magick;
-        if (my $err = $image->Read("$obj->{'final'}->{$f}->{'dir'}$obj->{'final'}->{'name'}.jpg")){
-            warn "[$obj->{'final'}->{$f}->{'dir'}$obj->{'final'}->{'name'}.jpg] Failed to read, $err";
+        if (my $err = $image->Read("$obj->{'final'}->{$_}->{'dir'}$obj->{'final'}->{'name'}.jpg")){
+            warn "[$obj->{'final'}->{$_}->{'dir'}$obj->{'final'}->{'name'}.jpg] Failed to read, $err";
             return 0;
         }
         if (my $err = $image->Strip()){
-            warn "[$obj->{'final'}->{$f}->{'dir'}$obj->{'final'}->{'name'}.jpg] Failed to strip, $err";
+            warn "[$obj->{'final'}->{$_}->{'dir'}$obj->{'final'}->{'name'}.jpg] Failed to strip, $err";
             return 0;
         }
-        if (my $err = $image->Write(filename => "$obj->{'final'}->{$f}->{'dir'}$obj->{'final'}->{'name'}.jpg", quality => 90)) {
-            warn "[$obj->{'final'}->{$f}->{'dir'}$obj->{'final'}->{'name'}.jpg] Failed to write, $err";
+        if (my $err = $image->Write(filename => "$obj->{'final'}->{$_}->{'dir'}$obj->{'final'}->{'name'}.jpg", quality => 90)) {
+            warn "[$obj->{'final'}->{$_}->{'dir'}$obj->{'final'}->{'name'}.jpg] Failed to write, $err";
             return 0;
         }
     }
@@ -202,13 +224,13 @@ sub integrity {
 
     lock $obj;
 
-    foreach my $f (split(',',$obj->{'options'}->{'format'})) {
+    foreach (keys %{ $main::options{'format'} }) {
+        next if $main::options{'format'}{$_}{'type'} ne 'video';
+        next if defined $obj->{'final'}->{$_}->{'exist'} and $main::options{'overwrite'} eq 'false';
 
-        next if defined $obj->{'final'}->{$f}->{'exist'} and not defined $obj->{'options'}->{'overwrite'};
+        print "[$obj->{'final'}->{$_}->{'path'}] Checking integrity...\n" if $main::options{'verbose'} eq 'true';
 
-        print "[$obj->{'final'}->{$f}->{'path'}] Checking integrity...\n" if defined $obj->{'options'}->{'verbose'};
-
-        &execute($obj->{'final'}->{$f}->{'path'}, 'File is corrupted', "ffmpeg -loglevel error -i $obj->{'final'}->{$f}->{'path'} -f null -");
+        &execute($obj->{'final'}->{$_}->{'path'}, 'File is corrupted', "ffmpeg -loglevel error -i $obj->{'final'}->{$_}->{'path'} -f null -");
     }
 
     return 1;
@@ -255,6 +277,9 @@ sub execute {
             return 0;
         }
     }
+
+    # ffmpeg messes up terminal, this is a workaround
+    system('stty sane');
 
     return 1;
 }
